fd9cba912ae886808194d2ec7f71535bbfdf2920
James Wildbure <jamesw986@gmail.com>
2022-02-18 20:35:39 +0000

    add basic view for home page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 app_name = 'learning_logs'
-urlspatterns = [
+urlpatterns = [
     # Home page
     path('', views.index, name='index'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    """ THe home page for Learning Log """
+    """ The home page for Learning Log """
     return render(request, 'learning_logs/index.html')
