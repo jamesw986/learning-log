2692af56f01004e46d7658551e272ef0d366fb61
James Wildbure <jamesw986@gmail.com>
2022-03-04 17:23:33 +0000

    added exception for invalid form so server error is not thrown

--- users/views.py
+++ users/views.py
@@ -11,11 +11,14 @@ def register(request):
         # Process completed form
         form = UserCreationForm(data=request.POST)
         
-        if form.is_valid:
-            new_user = form.save()
-            # Log the user in and then redirect to home page
-            login(request, new_user)
-            return redirect('learning_logs:index')
+        try:
+            if form.is_valid:
+                new_user = form.save()
+                # Log the user in and then redirect to home page
+                login(request, new_user)
+                return redirect('learning_logs:index')
+        except ValueError:
+            pass
 
     # Display a blank or invalid form
     context = {'form': form}
